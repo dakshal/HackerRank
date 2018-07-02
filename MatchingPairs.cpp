356e0899c362a5387b9af277525c55a0415e9a91
Dakshal Shah <shahdakshal95@gmail.com>
2018-07-01 23:29:30 -0400

    reformatted code for better viewability

--- MatchingPairs.cpp
+++ MatchingPairs.cpp
@@ -1,21 +1,22 @@
 int matchingpairs(string str) {
 	if (!isUpperCase(str.at(0)) || str.size() == 0)
 		return -1;
-char array[str.size()];
+	char array[str.size()];
 	int pointer = 1;
-array[0] = str.at(0);
+	array[0] = str.at(0);
+	
 	int res = -1;
 	for (int i = 1; i < str.size(); i++) {
 		if (!isUpperCase(str.at(i))) {
 			char c = array[pointer-1];
 			if (c + 32 == str.at(i)) {
-      pointer--;
+      			pointer--;
 				res = i;
 			} else
 				return res;
 		} else {
-    array[pointer] = str.at(i);
-    pointer++;
+		    array[pointer] = str.at(i);
+		    pointer++;
 		}
 	}
 	if (pointer == 0)
