08a694a9cad569b913218ec8f6d1a11eb68ddc96
Dakshal Shah <shahdakshal95@gmail.com>
2018-07-02 23:26:43 -0400

    Algo to find min Swap required to sort an array O(N^2)

---
+++ MinSwap.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int count = 0;
+        for(int i=0 ;i<arr.length; i++){
+            int pos = i;
+            for(int j=i+1; j< arr.length; j++){
+                if(arr[j]<arr[pos]){
+                    pos = j;
+                }
+            }
+            if(i!=pos){
+                int min = arr[pos];
+                arr[pos] = arr[i];
+                arr[i] = min;
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
