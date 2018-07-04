1a69da69cc57c2c6beb666ae0856ee8e7ac19d38
Dakshal Shah <shahdakshal95@gmail.com>
2018-07-04 13:15:25 -0400

    Array Manipulation Hacker Rank needs output verification

---
+++ ArrayManipulation.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries) {
+        
+        int [] sum = new int[n+1];
+        
+        int max = 0;
+        for(int i=0; i<queries.length; i++){
+            sum[queries[i][0]-1] += queries[i][2];
+            if(queries[i][1]<=n) sum[queries[i][1]] -= queries[i][2];
+        }
+        int tempSum = 0;
+        
+        for(int i=0; i<sum.length; i++){
+            System.out.print("\t"+sum[i]);
+            tempSum += sum[i];
+            max = Math.max(max, tempSum);
+        }
+        return max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[][] queries = new int[m][3];
+
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowItems[j]);
+                queries[i][j] = queriesItem;
+            }
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
