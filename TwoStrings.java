b33fb41cf8780c357cf1ce460402da858bcdbf00
Dakshal Shah <shahdakshal95@gmail.com>
2018-07-05 23:58:38 -0400

    Two Strings modified for all test cases

--- TwoStrings.java
+++ TwoStrings.java
@@ -10,25 +10,34 @@ public class TwoStrings {
 
     // Complete the twoStrings function below.
     static String twoStrings(String s1, String s2) {
-    	
-    	int m = s1.length();
-    	int n = s2.length();
-    	
-    	if(m>n) {
-    		for(int i=0; i<n; i++) {
-    			if(s2.contains(""+s1.charAt(n))) {
-    				return "Yes";
-    			}
-    		}
-    	} else {
-    		for(int i=0; i<m; i++) {
-    			if(s1.contains(""+s2.charAt(n))) {
-    				return "Yes";
-    			}
-    		}
-    	}
-    	
-        return "No";
+                
+        String charArray [] = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        
+        String strS1 = "";
+        for(int i=0; i<26; i++) {
+            if(s1.contains(charArray[i])) {
+                strS1 += charArray[i];
+            }
+        }
+        
+        String strS2 = "";
+        for(int i=0; i<26; i++) {
+            if(s2.contains(charArray[i])) {
+                strS2 += charArray[i];
+            }
+        }
+                
+        if(strS1.length() + strS2.length()>26) {
+            return "YES";
+        } else {
+            for(int i=0; i<strS2.length(); i++) {
+                if(strS1.contains(""+strS2.charAt(i))) {
+                    return "YES";
+                }
+            }
+        }
+                
+        return "NO";
     }
 
     private static final Scanner scanner = new Scanner(System.in);
