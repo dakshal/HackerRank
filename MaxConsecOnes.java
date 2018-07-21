5c20eb0df9d5f4aa6689398616c8cb4d6e134f4d
Dakshal Shah <shahdakshal95@gmail.com>
2018-07-20 23:31:19 -0400

    minor modification to optimize

--- MaxConsecOnes.java
+++ MaxConsecOnes.java
@@ -1,23 +1,26 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
+        // System.out.println(nums.length);
         int count = 0, secCount = 0, max = 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == 1){
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == 1) {
                 count++;
                 secCount++;
-            } else if(nums[i] == 0){
-                if(count != secCount){
+            } else if(nums[i] == 0) {
+                // System.out.println(count+"\t"+secCount+"\t"+max);
+                if(count != secCount) {
                     if(count>max){
                         max = count;
                     }
-                    count = secCount;
+                    count = secCount+1;
                     secCount = 0;
                 } else {
                     count++;
+                    secCount = 0;
                 }
             }
         }
-        if(max<count){
+        if(max<count) {
             max = count;
         }
         return max;
