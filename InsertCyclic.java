94b5f79452a3e79928c4a811dca35222d7f385aa
Dakshal Shah <shahdakshal95@gmail.com>
2018-07-22 16:29:53 -0400

    optimized for cyclic testcases

--- InsertCyclic.java
+++ InsertCyclic.java
@@ -15,19 +15,48 @@ class Node {
 class Solution {
     public Node insert(Node head, int insertVal) {
         if(head == null){
+            // System.out.println("head is null:");
             return new Node(insertVal, null);
         }
         
         if(head.next == head){
+            // System.out.println("head is same as next");
             head.next = new Node(insertVal, head);
             return head;
         }
         
-        while(!(head.val <= insertVal && head.next.val >= insertVal)){
-            head = head.next;
+        Node next = head.next;
+        Node prev = head;
+        Node least = head;
+        Node highest = head;
+        // System.out.println("Initial Params:- "+prev.val+"\t"+next.val+"\t"+insertVal);
+        
+        while((next != head) && ((prev.val < insertVal && next.val < insertVal) || (prev.val > insertVal && next.val > insertVal))){
+        // System.out.println("Moving Next:- "+prev.val+"\t"+next.val+"\t"+insertVal);
+            if(least.val > next.val) {
+                least = next;
+            }
+            if(highest.val < next.val) {
+                highest = next;
+            }
+            prev = next;
+            next = next.next;
         }
-        Node nextNode = new Node(insertVal, head.next);
-        head.next = nextNode;
-        return head;
+        // System.out.println("Done with While:- "+prev.val+"\t"+next.val+"\t"+head.val+"\t"+insertVal);
+        if(next == head) {
+            Node nextNode = new Node(insertVal, highest.next);
+            highest.next = nextNode;
+            return head;
+        } else {
+            if(next.val < insertVal){
+                Node nextNode = new Node(insertVal, next.next);
+                next.next = nextNode;
+                return head;
+            } else {
+                Node nextNode = new Node(insertVal, prev.next);
+                prev.next = nextNode;
+                return head;
+            }
+        }
     }
 }
