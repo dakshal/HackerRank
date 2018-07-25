2962965a8b67f547229b23d0a66c2a8696e98a69
Dakshal Shah <shahdakshal95@gmail.com>
2018-07-25 08:26:42 -0400

    improved calculation for decoding

--- DecodeWays.java
+++ DecodeWays.java
@@ -1,23 +1,23 @@
 class Solution {
-    public int numDecodings(String s) {
+    public int numDecodings(String s) {        
         char[] ch = s.toCharArray();
         int dp[] = new int[s.length()+1];
         dp[ch.length] = 1;
+        // dp[ch.length-1] = 1;
         for(int i=ch.length-1; i>=0; i--){
             if(ch[i]>48 && ch[i]<58) {
                 if(i<ch.length -1){
                     int num = Integer.parseInt(ch[i]+""+ch[i+1]);
-                    if(num>=27 || num == 10 || num == 20){
-                        dp[i] = dp[i+1] + 1;
+                    if(num>=27){
+                        dp[i] = dp[i+1];
                     } else {
-                        dp[i] = dp[i+1] +dp[i+2]+2;
+                        dp[i] = dp[i+1] +dp[i+2];
                     }
                 } else {
-                    dp[i] = dp[i+1] + 1;
+                    dp[i] = dp[i+1];
                 }
-                // dp[i] +=count;
             } else {
-                dp[i] = dp[i+1];
+                // dp[i] = dp[i+1];
             }
         }
         
