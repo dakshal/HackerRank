2473ee308879b9da6e31d14fe89eacbd7dc941c7
Dakshal Shah <shahdakshal95@gmail.com>
2018-08-03 13:34:47 -0400

    BruteForce Algorithm for finding shortest Palindrome

--- FindShortestPalindrome.java
+++ FindShortestPalindrome.java
@@ -4,71 +4,65 @@ class Solution {
             return s;
         }
         char[] word = s.toCharArray();
-        int center, max = 0, n = word.length, count = 0;
-        center = (n%2==0)?((int) Math.ceil(n/2)-1):((int) Math.ceil(n/2));
+        int n= word.length;
+        int mid = (0+n)/2;
+        int count=0, center=0, type = 2;
+        while(mid-1>=0){
+            int c1 = findPalindrome(word, mid, true, 1);
+            int c2 = findPalindrome(word, mid, false, 1);
+            if((mid-c2+1)<0){
+                count = c2;
+                center = mid;
+                type = 1;
+                break;
+            } else if(mid-c1<0){
+                count = c1;
+                center = mid;
+                type = 0;
+                break;
+            } 
+            mid -= 1;
+        }
         StringBuilder sb = new StringBuilder();
-        boolean odd, even, found = false;
-        
-        while(center>=0){
-            count = 1;
-            odd = true;
-            even = true; 
-            System.out.println("c:- "+center+"\tco:- "+count);
-            if(center - count<0){
-                count++;
+        if(count == 0 && center==0){
+            for(int i=n-1; i>1; i--){
+                sb.append(word[i]);
             }
-            while((center-count)>=0 && (center+count)<n){
-                System.out.println(center+"\t"+count+"\t"+word[center-count]+"\t"+word[center+count]+"\t"+word[center-count+1]+"\t"+odd+"\t"+even);
-                if(word[center-count] == word[center+count] && odd){
-                    
-                    
-                } else {
-                    odd = false;
-                }
-                if(word[center-count+1] == word[center+count] && even){
-                    
-                } else {
-                    even = false;
-                }
-                count++;
-                if(!odd && !even){
-                    System.out.println("breaking loop!!!");
-                    break;
-                }
+            if(word[0]!=word[1]){
+                sb.append(word[1]);
+            } 
+        } else {
+            // System.out.println(center+"\t\t"+count+"\t\t"+type);
+            for(int i=n-1; i>=(center+count); i--){
+                sb.append(word[i]);
             }
-            if((center-count)<=0 && (odd || even)) {
-                count--;
-                count = (count<=0)?(1):count;
-                System.out.println(center+"\t"+count+"\t"+(center-count)+"\t"+odd+"\t"+even);
-                if((center-count)>=0 && center+count<n && odd) {
-                    if(word[center-count] == word[center+count]) {
-                        center = center+count;
-                        break;
-                    }
-                }
-                if((center-count+1)>=0 && center+count<n && even){
-                    if(word[center-count+1] == word[center+count]) {
-                        center = center+count;
-                        break;
-                    }
-                }
-                if(center+count<n){
-                    center = center+count-1;
-                    System.out.println("breaking\t"+count+"\t\t"+center);
-                    break;
-                }
-            }
-            center--;
         }
-        int j = n-1;
-        System.out.println(j+"\t"+center+"\t");
-        while(j>center){
-            sb.append(word[j]);
-            j--;
-        }
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<n; i++){
             sb.append(word[i]);
         }
         return sb.toString();
+    }
+    
+    public int findPalindrome(char[] word, int start, boolean type, int count) {
+        if(type){
+            if(start-count<0 || start+count>=word.length) {
+                return count;
+            }
+            if(word[start-count] == word[start+count]) {
+                return findPalindrome(word, start, type, count+1);
+            } else {
+                return count;
+            }
+        } else {
+            if(start-count+1<0 || start+count>=word.length) {
+                return count;
+            }
+            if(word[start-count+1] == word[start+count]) {
+                return findPalindrome(word, start, type, count+1);
+            } else {
+                return count;
+            }
+            
+        }
     }
 }
