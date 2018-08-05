8c5849c5cec928040826b8d54da2e0d2166d9c30
Dakshal Shah <shahdakshal95@gmail.com>
2018-08-05 14:41:49 -0400

    Peeking Number Iterator optimized LeetCode java

--- PeekingIterator.java
+++ PeekingIterator.java
@@ -1,32 +1,40 @@
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 class PeekingIterator implements Iterator<Integer> {
-		
-		ArrayList<Integer> list = new ArrayList<>();
-		int pos = 0;
+    
+    Iterator<Integer> iterator;
+    Integer cur = null;
 
-		public PeekingIterator(Iterator<Integer> iterator) {
-		    // initialize any member here.
-			while(iterator.hasNext()) {
-				list.add(iterator.next());
-			}
-		}
-
-	    // Returns the next element in the iteration without advancing the iterator.
-		public Integer peek() {
-			return list.get(pos);	        
-		}
-
-		// hasNext() and next() should behave the same as in the Iterator interface.
-		// Override them if needed.
-		@Override
-		public Integer next() {
-			return list.get(pos++);		    
-		}
-
-		@Override
-		public boolean hasNext() {
-			if(pos<list.size()) {
-				return true;
-			}
-			return false;		    
-		}
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    // initialize any member here.	  
+        this.iterator = iterator;
 	}
+
+    // Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+        if(cur == null){
+            cur = iterator.next();
+        }
+        return cur;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+	    if(cur==null){
+            return iterator.next();
+        }
+        Integer ret = cur;
+        cur = null;
+        return ret;
+	}
+
+	@Override
+	public boolean hasNext() {
+	    if(cur==null){
+            return iterator.hasNext();
+        }
+        return true;
+	}
+}
